ed1bcf02c5277d83825a891d7d061a13f9341323
github282004 <leeplata03@gmail.com>
2024-02-23 07:54:55 +0800

    Add project files.

---
+++ ComputeAverageApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter 5 grades seperated by new line:");
+            double sum = 0;
+            int x = 0;
+
+            while(x < 5)
+            {            
+                double grade = Convert.ToDouble(Console.ReadLine());
+                sum += grade;
+                x++;
+            }
+            double Average = sum / 5;
+            double roundedAverage = Math.Round(Average, 2);
+            Console.WriteLine($"Average is: " + Average + " and round off to " + Math.Round(Average));
+        }
+    }
+}
